5282dd3ade18aa83655b659fcefeab5f17d953b9
- <karinateyssere@gmail.com>
2024-03-30 10:15:44 +0100

    Offline and redux, jwt, router packages

---
+++ src/Offline.jsx
@@ -0,0 +1,32 @@
+import { useEffect } from "react";
+import { FaPlane, FaWifi } from "react-icons/fa";
+import { useBooleanState, usePrevious } from "webrix/hooks";
+
+export default function Offline({ children }) {
+  const {
+    value: online,
+    setFalse: setOffline,
+    setTrue: setOnline,
+  } = useBooleanState(navigator.onLine);
+  const previousOnline = usePrevious(online);
+
+  useEffect(() => {
+    if (!online) {
+      //console.log("Offline");
+    } else {
+      //console.log("Online");
+    }
+  }, [online]);
+
+  useEffect(() => {
+    window.addEventListener("online", setOnline);
+    window.addEventListener("offline", setOffline);
+
+    return () => {
+      window.removeEventListener("online", setOnline);
+      window.removeEventListener("offline", setOffline);
+    };
+  }, []);
+
+  return <>{online ? <div><FaWifi/></div> : <div><FaPlane/></div>}</>;
+}
